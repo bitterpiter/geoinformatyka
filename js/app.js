738351e1284b9a19411b3726ae1a96d10ba0785c
bitterpiter <bitter.piter@gmail.com>
2015-01-19 20:08:59 +0000

    zajecia z edycji

--- js/app.js
+++ js/app.js
@@ -1,8 +1,46 @@
-angular.module('myApp', [])
-    .controller('myCtrl', function($scope, $http) {
-        var url = 'https://gdata.youtube.com/feeds/api/users/orbitalofficial/uploads?alt=json-in-script&callback=JSON_CALLBACK';
-        $http.jsonp(url)
-            .success(function(data) {
-                $scope.results = data.feed.entry;
-            }); 
+angular.module('myApp', ["firebase"])
+    .constant('FIREBASE_URI', "https://website-techs.firebaseio.com/")
+    .controller("myCtrl", function($scope, ItemsService) {
+        
+        $scope.items = ItemsService.getItems();
+        console.log($scope.items);
+        $scope.addItem = function(text) {
+            ItemsService.addItem({text: text});
+            delete $scope.newItemText;
+        }
+        $scope.updateItem = function (id) {
+            ItemsService.updateItem(id);
+        };
+    
+        $scope.removeItem = function (id) {
+            ItemsService.removeItem(id);
+        };
+    })
+    .factory("ItemsService", function($firebase, FIREBASE_URI) {
+        var ref = new Firebase(FIREBASE_URI+"/items");
+        var sync = $firebase(ref);
+        var items = sync.$asArray();
+        
+        var getItems = function() {
+            return items;
+        }
+        
+        var addItem = function(item) {
+            items.$add(item);
+        }
+        
+        var removeItem = function(id) {
+            items.$remove(id);
+        }
+        
+        var updateItem = function (id) {
+            items.$save(id);
+        };
+        
+        return {
+            getItems: getItems,
+            addItem: addItem,
+            removeItem: removeItem,
+            updateItem: updateItem
+        }
     });
